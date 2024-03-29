ef88f740a2d1aad4abbc91807a12d598e03f3ee9
Munarbek <149784826+Dayons@users.noreply.github.com>
2024-03-29 19:12:58 +0600

    first commit

---
+++ src/main/java/com/example/test/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.test.service;
+
+import com.example.test.entity.Role;
+import com.example.test.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+
+public interface UserService {
+    User saveUser(User user);
+
+    Role saveRole(Role role);
+
+    void addRoleToUser(String username, String roleName);
+
+    User getUser(String username);
+
+    List<User> getUsers();
+}
